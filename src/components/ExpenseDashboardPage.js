554a1ed5ba375d9ef17ae1c7ee77d6d345e2c6d2
Unknown <chatajazzy@interia.pl>
2018-05-15 13:22:21 +0200

    Include filters in dashboard component

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import ExpensesList from './ExpensesList';
+import ExpensesListFilters from './ExpensesListFilters';
 
 export default class ExpenseDashboardPage extends Component {
   render() {
     return (
       <div>
         <ExpensesList />
+        <ExpensesListFilters />
       </div>
     );
   }
