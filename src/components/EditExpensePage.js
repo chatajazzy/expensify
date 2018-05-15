9eeab406211b47107eb4394b0dc23d4e51e1ca63
Unknown <chatajazzy@interia.pl>
2018-05-15 14:19:40 +0200

    Add edit expense page with posibility to remove expense

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as expensesActions from '../actions/expensesActions';
+import ExpenseForm from './ExpenseForm';
 
-export default class EditExpensePage extends Component {
-  render() {
-    console.log(this.props);
+const EditExpensePage = props => {
+  const { id, dispatch } = props;
 
-    return <div>EditExpensePage</div>;
-  }
+  return (
+    <div>
+      <ExpenseForm
+        expense={props.expense}
+        onFormSubmit={expense => {
+          props.dispatch(
+            expensesActions.editExpense(props.expense.id, expense)
+          );
+          props.history.push('/');
+        }}
+      />
+      <button
+        onClick={() => {
+          dispatch(expensesActions.removeExpense({ id }));
+          props.history.push('/');
+        }}
+      >
+        x
+      </button>
+    </div>
+  );
+};
+
+function mapStateToProps(state, ownProps) {
+  const id = ownProps.match.params.id;
+  return {
+    id: id,
+    expense: state.expenses.find(expense => {
+      return expense.id === id;
+    })
+  };
 }
+
+export default connect(mapStateToProps)(EditExpensePage);
