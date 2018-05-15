c1db49fef5bb1c7d1dbc5722d846c667fc7bb72b
Unknown <chatajazzy@interia.pl>
2018-05-15 14:15:47 +0200

    Add adding expense page

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as expensesActions from '../actions/expensesActions';
+
 import ExpenseForm from './ExpenseForm';
 
-export default class AddExpensePage extends Component {
-  render() {
-    return (
-      <div>
-        <h1>AddExpensePage</h1>
-        <ExpenseForm />
-      </div>
-    );
-  }
-}
+const AddExpensePage = props => {
+  return (
+    <div>
+      <h1>AddExpensePage</h1>
+      <ExpenseForm
+        onFormSubmit={expense => {
+          props.dispatch(expensesActions.addExpense(expense));
+          props.history.push('/');
+        }}
+      />
+    </div>
+  );
+};
+
+export default connect()(AddExpensePage);
