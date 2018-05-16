48c61d6fc832ad0c1be85bed9b40a35e7b8b16dd
Unknown <chatajazzy@interia.pl>
2018-05-16 13:41:05 +0200

    Remove useless console.log from auth reducer

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -4,7 +4,6 @@ import { initialState } from './initialState';
 export default (state = initialState.auth, action) => {
   switch (action.type) {
     case appConstants.LOGIN:
-      console.log('object', action.uid);
       return {
         uid: action.uid
       };
